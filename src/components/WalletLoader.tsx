a3391c27dacf0d2301d91d184810a381e2df4454
Pratyaksh Gupta ᵍᵐ <pratyaksh1615@gmail.com>
2022-05-02 22:50:12 +0000

    remove chrome extension wallet connect

--- src/components/WalletLoader.tsx
+++ src/components/WalletLoader.tsx
@@ -37,23 +37,16 @@ function WalletLoader({
     return (
       <div className="max-w-full">
         <h1 className="text-6xl font-bold">Welcome</h1>
-        <div className="flex flex-wrap items-center justify-around md:max-w-4xl mt-6 sm:w-full">
-          <button
-            className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus"
-            onClick={() => connect(ConnectType.WALLETCONNECT)}
-          >
-            <h3 className="text-2xl font-bold">Chrome extension &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Manage your multsig by connecting your Terra Station wallet.
-            </p>
-          </button>
-        </div>
+        <div className="flex flex-wrap items-center justify-around md:max-w-4xl mt-6 sm:w-full"></div>
         <div className="flex flex-wrap items-center justify-around md:max-w-4xl mt-6 sm:w-full">
           <button
             className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus"
             onClick={() => connect(ConnectType.WALLETCONNECT)}
           >
             <h3 className="text-2xl font-bold">WalletConnect &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Manage Your Legal Contracts By Connecting Your CosmDocs Ledger.
+            </p>
           </button>
         </div>
       </div>
